2b7a0f3f3126c4e290fecaf30558d8eb8f69de1f
AnqurVanillapy <anqurvanillapy@gmail.com>
2016-11-14 01:11:28 +0800

    updated _update(), _commit()

--- src/entry.js
+++ src/entry.js
@@ -1,6 +1,7 @@
 'use strict'
 
 import fs from 'fs'
+import readline from 'readline'
 
 const BLOCKSIZE = 512
 const ENCODING = 'ascii'
@@ -10,7 +11,7 @@ class PrelDB {
     this._mode = mode
 
     // The directory file, whose each line looks like
-    //    "key, (pos, siz)"
+    //    "key, [pos, siz]"
     // that points to the data file
     this._dirfile = fbname + '.dir'
 
@@ -21,11 +22,12 @@ class PrelDB {
     this._bakfile = fbname + '.bak'
 
     // The in-memory mapping that mirrors the directory file
-    this._index = undefined // maps key to a (pos, siz) pair
+    this._index = undefined // maps key to a [pos, siz] pair
 
     // Handle the creation
     this._create(flag)
     this._update()
+    this._commit()
   }
 
   _create (flag) {
@@ -35,28 +37,56 @@ class PrelDB {
       })
     }
 
-    fs.readFile(this._datfile, ENCODING, (err, data) => {
-      if (err) {
-        fs.writeFile(this._datfile, ENCODING, '', err => {
-          if (err) throw err
-          fs.chmod(this._datfile, this._mode)
-        })
-      }
+    fs.writeFile(this._datfile, '', err => {
+      if (err) { /* eat errors */ }
+      fs.chmod(this._datfile, this._mode)
     })
   }
 
+  // Read the directory to the in-memory index object
   _update () {
     this._index = {}
+    var rs = fs.createReadStream(this._dirfile, {encode: ENCODING})
 
-    fs.readFile(this._dirfile, ENCODING, (err, data) => {
-      if (err) { /* eat errors */ }
-      /* read data line by line */
+    rs.on('error', _ => {
+      return // eat errors
+    })
+
+    const rl = readline.createInterface({
+      input: rs
+    })
+
+    rl.on('line', (line) => {
+      // Destructure a nested Array from a line
+      var [key, pair] = JSON.parse('[' + line.trim() + ']')
+      this._index[key] = pair // pos and siz pair
     })
   }
 
+  // Write the index object to the directory file. The original directory file
+  // (if any) is renamed with .bak extension for backup first. If a backup
+  // exists, it's deleted.
   _commit () {
+    if (typeof this._index === 'undefined') return // it's closed
 
+    fs.unlink(this._bakfile, _ => { /* eat errors */ })
+    fs.rename(this._dirfile, this._bakfile, _ => { /* safe backup */ })
+
+    fs.open(this._dirfile, 'w', err => {
+      if (err) throw err
+
+      fs.chmod(this._dirfile, this._mode)
+      var ws = fs.createWriteStream(this._dirfile, {defaultEncoding: ENCODING})
+
+      Object.keys(this._index).forEach(k => {
+        ws.write(k.toString() + ',[' + this._index[k].toString() + ']')
+      })
+
+      ws.end('\n')
+    })
   }
+
+  sync () { this._commit() }
 
   _verifyOpen () {
     if (typeof this._index === 'undefined') {
@@ -64,8 +94,15 @@ class PrelDB {
     }
   }
 
+  get (key) {
+    this._verifyOpen()
+
+    // Destructuring can raise TypeError if no matches
+    var [pos, siz] = this._index[key]
+  }
+
   close () {
-    /* close the db */
+    // close the db
   }
 }
 
